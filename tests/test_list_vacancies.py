ca235822f4fbf06bbb819014216321e3372ce40c
Serg_Nout <kuchukov.sergey@gmail.com>
2024-11-23 12:49:14 +0300

    abctract class GetApi

--- tests/test_list_vacancies.py
+++ tests/test_list_vacancies.py
@@ -1,13 +1,47 @@
 from src.list_vacancies import ListVacansies
+import pytest
+
+from unittest.mock import patch
+from src.get_api import GetAPI
+
+@pytest.mark.parametrize("get_api", [{"items":[{"id":"93353083","premium":False,"name":"Дворник"},]}
+                                     ])
 
 
-def test_list_vacancies(data_api, vacansies_data_0) -> None:
-    result = ListVacansies.tp_dict(data_api)
-    #print(result)
+@patch("requests.get")
+def test_list_vacancies(mock_get, get_api):
+
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.json.return_value = get_api
+    test_object = ListVacansies('бухгалтер')
+    result = test_object.to_dict()
+
+    assert  test_object.data == [
+        {'id': '93353083', 'premium': False, 'name': 'Дворник'},
+        {'id': '93353083', 'premium': False, 'name': 'Дворник'},
+        {'id': '93353083', 'premium': False, 'name': 'Дворник'},
+        {'id': '93353083', 'premium': False, 'name': 'Дворник'},
+        {'id': '93353083', 'premium': False, 'name': 'Дворник'}
+    ]
+    assert test_object.status == 200
+
+    assert result == [{'_Vacancy__id_v': '93353083', '_Vacancy__name': 'Дворник', '_Vacancy__salary_from': 0, '_Vacancy__salary_to': 0,
+      '_Vacancy__currency': '', '_Vacancy__url': None, '_Vacancy__date': None,
+      '_Vacancy__additionally': {'snippet': '', 'schedule': '', 'address': ''}},
+     {'_Vacancy__id_v': '93353083', '_Vacancy__name': 'Дворник', '_Vacancy__salary_from': 0, '_Vacancy__salary_to': 0,
+      '_Vacancy__currency': '', '_Vacancy__url': None, '_Vacancy__date': None,
+      '_Vacancy__additionally': {'snippet': '', 'schedule': '', 'address': ''}},
+     {'_Vacancy__id_v': '93353083', '_Vacancy__name': 'Дворник', '_Vacancy__salary_from': 0, '_Vacancy__salary_to': 0,
+      '_Vacancy__currency': '', '_Vacancy__url': None, '_Vacancy__date': None,
+      '_Vacancy__additionally': {'snippet': '', 'schedule': '', 'address': ''}},
+     {'_Vacancy__id_v': '93353083', '_Vacancy__name': 'Дворник', '_Vacancy__salary_from': 0, '_Vacancy__salary_to': 0,
+      '_Vacancy__currency': '', '_Vacancy__url': None, '_Vacancy__date': None,
+      '_Vacancy__additionally': {'snippet': '', 'schedule': '', 'address': ''}},
+     {'_Vacancy__id_v': '93353083', '_Vacancy__name': 'Дворник', '_Vacancy__salary_from': 0, '_Vacancy__salary_to': 0,
+      '_Vacancy__currency': '', '_Vacancy__url': None, '_Vacancy__date': None,
+      '_Vacancy__additionally': {'snippet': '', 'schedule': '', 'address': ''}}]
 
 
-
-    assert vacansies_data_0 == result
 
 
 
